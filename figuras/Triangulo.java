967e67e3b6f7a740cdf075df68841ae66092f0c9
demonassit <e.ladrillero.h985@edu.utc.mx>
2023-10-24 20:55:15 -0600

    Figuras

---
+++ figuras/Triangulo.java
@@ -0,0 +1,26 @@
+public class Triangulo extends Figura {
+ 
+	private double base;
+	private double altura;
+	private double lado1;
+	private double lado2;
+	private double lado3;
+ 
+	public Triangulo(double base, double altura, double lado1, double lado2, double lado3) {
+		this.base = base;
+		this.altura = altura;
+		this.lado1 = lado1;
+		this.lado2 = lado2;
+		this.lado3 = lado3;
+		calcularArea();
+		calcularPerimetro();
+	}
+
+	protected void calcularArea() {
+		area = base * altura / 2;
+	}
+ 
+	protected void calcularPerimetro() {
+		perimetro = lado1 + lado2 + lado3;
+	}
+}
